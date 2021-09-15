41b2545e8b0c19aa67fb06815d011e064c6c004e
CPU20_HCM <CPU20_HCM@CPU_20ANTRAN>
2021-09-15 08:28:14 +0700

    Add project files.

---
+++ Presentation.Main/Controllers/UserRolesController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Services.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Main.Controllers
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class UserRolesController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index(string userId)
+        {
+            var viewModel = new List<UserRolesViewModel>();
+            var user = await _userManager.FindByIdAsync(userId);
+            foreach (var role in _roleManager.Roles)
+            {
+                var userRolesViewModel = new UserRolesViewModel
+                {
+                    RoleName = role.Name
+                };
+                    userRolesViewModel.Selected = false;
+                viewModel.Add(userRolesViewModel);
+            }
+            var model = new ManageUserRolesViewModel()
+            {
+                UserId = userId,
+                UserRoles = viewModel
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Update(string id, ManageUserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            result = await _userManager.AddToRolesAsync(user, model.UserRoles.Where(x => x.Selected).Select(y => y.RoleName));
+            var currentUser = await _userManager.GetUserAsync(User);
+            await _signInManager.RefreshSignInAsync(currentUser);
+            await Business.Services.Seeds.DefaultUsers.SeedSuperAdminAsync(_userManager, _roleManager);
+            return RedirectToAction("Index");
+        }
+    }
+}
